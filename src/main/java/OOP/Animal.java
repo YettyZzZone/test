bef57642d6e482d612b9f584cd8a071f168c92c1
Зворыгин Дмитрий <dzvorygin@htc-cs.ru>
2020-12-06 20:25:03 +0400

    Add Animal class

---
+++ src/main/java/OOP/Animal.java
@@ -0,0 +1,9 @@
+package OOP;
+
+public abstract class Animal {
+    String name;
+    int hungryStatus;
+    int weight;
+    abstract void walk();
+    abstract void eat();
+}
--- src/main/java/runner/Runner.java
+++
@@ -1,7 +0,0 @@
-package runner;
-
-public class Runner {
-    public static void main(String[] args) {
-        System.out.println("Hello");
-    }
-}
