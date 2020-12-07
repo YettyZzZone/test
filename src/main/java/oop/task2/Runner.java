e1d1771ea4db20c70a1f4405393130cde1ee9670
Зворыгин Дмитрий <dzvorygin@htc-cs.ru>
2020-12-07 16:31:32 +0400

    Sorting by animal parameters is implemented

--- src/main/java/oop/task2/Runner.java
+++ src/main/java/oop/task2/Runner.java
@@ -1,22 +1,25 @@
 package oop.task2;
 
+import oop.task1.Animal;
 import oop.task1.сat.Cat;
 import oop.task2.creating_and_showing.CreateArrayList;
 import oop.task2.creating_and_showing.ShowingAnimals;
 import oop.task2.show_only.ShowOnlyThisClass;
+import oop.task2.sort.Sorting;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Runner {
     public static void main(String[] args) {
-        Scanner user_choice = new Scanner(System.in);
+        Scanner userChoice = new Scanner(System.in);
         while (true) {
             System.out.println("Введи цифру необходимого тебе действия: \n" +
                     "1. Показать весь питомник\n" +
                     "2. Показать только выбранный вид животных\n" +
                     "3. Отсортировать коллекцию по выбранному параметру\n" +
                     "4. Вывести животное подходящее под заданные параметры");
-            int num = user_choice.nextInt();
+            int num = userChoice.nextInt();
             switch (num){
                 //Показываем весь питомник
                 case 1 :
@@ -24,7 +27,7 @@ public class Runner {
                     CreateArrayList menagerie = new CreateArrayList();
                     //Создаем экземпляр класса для дальнейшего вызова метода, показывающего весь питомник
                     ShowingAnimals showing = new ShowingAnimals();
-                    showing.showingAnimals(menagerie.createArrayList());
+                    showing.showingAnimals((ArrayList<Animal>) menagerie.createArrayList());
                     break;
                 //Показываем только выбранный вид животных
                 case 2:
@@ -33,11 +36,11 @@ public class Runner {
                             "2. Собаки\n" +
                             "3. Черепахи");
                     //Считываем пользовательский ввод
-                    int num2 = user_choice.nextInt();
+                    int userChoice2 = userChoice.nextInt();
                     ShowOnlyThisClass onlyThisClass = new ShowOnlyThisClass();
                     //В зависимости от выбора пользователя, прокидываем в метод
                     //число, соответствующее определенному классу животного
-                    switch (num2){
+                    switch (userChoice2){
                         case 1:
                             onlyThisClass.showOnlyThisClass(1);
                             break;
@@ -46,6 +49,27 @@ public class Runner {
                             break;
                         case 3:
                             onlyThisClass.showOnlyThisClass(3);
+                            break;
+                    }
+                case 3:
+                    System.out.println("Выбери, по какому параметру ты хочешь отсортировать коллекцию: \n" +
+                            "1. Имя\n" +
+                            "2. Уровень голода\n" +
+                            "3. Вес");
+                    //Считываем пользовательский ввод
+                    int userChoice3 = userChoice.nextInt();
+                    Sorting sorting = new Sorting();
+                    //В зависимости от выбора пользователя, прокидываем в метод
+                    //число, соответствующее определенному классу животного
+                    switch (userChoice3){
+                        case 1:
+                            sorting.sortingAnimals(1);
+                            break;
+                        case 2:
+                            sorting.sortingAnimals(2);
+                            break;
+                        case 3:
+                            sorting.sortingAnimals(3);
                             break;
                     }
             }
