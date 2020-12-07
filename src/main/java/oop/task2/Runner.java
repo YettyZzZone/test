31ed2d0755a247eae16a7fcfbb6c2890e91c52d8
Зворыгин Дмитрий <dzvorygin@htc-cs.ru>
2020-12-07 19:05:59 +0400

    Fixed bugs

--- src/main/java/oop/task2/Runner.java
+++ src/main/java/oop/task2/Runner.java
@@ -13,78 +13,82 @@ import java.util.Scanner;
 public class Runner {
     public static void main(String[] args) {
         Scanner userChoice = new Scanner(System.in);
-        while (true) {
-            System.out.println("Введи цифру необходимого тебе действия: \n" +
-                    "1. Показать весь питомник\n" +
-                    "2. Показать только выбранный вид животных\n" +
-                    "3. Отсортировать коллекцию по выбранному параметру\n" +
-                    "4. Вывести животное подходящее под заданные параметры");
-            int num = userChoice.nextInt();
-            switch (num){
-                //Показываем весь питомник
-                case 1 :
-                    //Создаем экземпляр класса для создания ArrayList
-                    CreateArrayList menagerie = new CreateArrayList();
-                    //Создаем экземпляр класса для дальнейшего вызова метода, показывающего весь питомник
-                    ShowingAnimals showing = new ShowingAnimals();
-                    showing.showingAnimals((ArrayList<Animal>) menagerie.createArrayList());
-                    break;
-                //Показываем только выбранный вид животных
-                case 2:
-                    System.out.println("Выбери, какой вид животных ты хочешь увидеть: \n" +
-                            "1. Кошки\n" +
-                            "2. Собаки\n" +
-                            "3. Черепахи");
-                    //Считываем пользовательский ввод
-                    int userChoice2 = userChoice.nextInt();
-                    ShowOnlyThisClass onlyThisClass = new ShowOnlyThisClass();
-                    //В зависимости от выбора пользователя, прокидываем в метод
-                    //число, соответствующее определенному классу животного
-                    switch (userChoice2){
-                        case 1:
-                            onlyThisClass.showOnlyThisClass(1);
-                            break;
-                        case 2:
-                            onlyThisClass.showOnlyThisClass(2);
-                            break;
-                        case 3:
-                            onlyThisClass.showOnlyThisClass(3);
-                            break;
-                    }
-                    break;
-                case 3:
-                    System.out.println("Выбери, по какому параметру ты хочешь отсортировать коллекцию: \n" +
-                            "1. Имя\n" +
-                            "2. Уровень голода\n" +
-                            "3. Вес");
-                    //Считываем пользовательский ввод
-                    int userChoice3 = userChoice.nextInt();
-                    Sorting sorting = new Sorting();
-                    //В зависимости от выбора пользователя, прокидываем в метод
-                    //число, соответствующее определенному классу животного
-                    switch (userChoice3){
-                        case 1:
-                            sorting.sortingAnimals(1);
-                            break;
-                        case 2:
-                            sorting.sortingAnimals(2);
-                            break;
-                        case 3:
-                            sorting.sortingAnimals(3);
-                            break;
-                    }
-                    break;
-                case 4:
-                    System.out.println("Введи начальную границу диапозона веса животного: ");
-                    //Считываем пользовательский ввод
-                    int choiceWeigthStart = userChoice.nextInt();
-                    System.out.println("Введи конечную границу диапозона веса животного: ");
-                    //Считываем пользовательский ввод
-                    int choiceWeigthEnd = userChoice.nextInt();
-                    ShowInterval showInterval = new ShowInterval();
-                    showInterval.showAnimalsFromInterval(choiceWeigthStart, choiceWeigthEnd);
-                    break;
+        try {
+            while (true) {
+                System.out.println("Введи цифру необходимого тебе действия: \n" +
+                        "1. Показать весь питомник\n" +
+                        "2. Показать только выбранный вид животных\n" +
+                        "3. Отсортировать коллекцию по выбранному параметру\n" +
+                        "4. Вывести животное подходящее под заданные параметры");
+                int num = userChoice.nextInt();
+                switch (num){
+                    //Показываем весь питомник
+                    case 1 :
+                        //Создаем экземпляр класса для создания ArrayList
+                        CreateArrayList menagerie = new CreateArrayList();
+                        //Создаем экземпляр класса для дальнейшего вызова метода, показывающего весь питомник
+                        ShowingAnimals showing = new ShowingAnimals();
+                        showing.showingAnimals((ArrayList<Animal>) menagerie.createArrayList());
+                        break;
+                    //Показываем только выбранный вид животных
+                    case 2:
+                        System.out.println("Выбери, какой вид животных ты хочешь увидеть: \n" +
+                                "1. Кошки\n" +
+                                "2. Собаки\n" +
+                                "3. Черепахи");
+                        //Считываем пользовательский ввод
+                        int userChoice2 = userChoice.nextInt();
+                        ShowOnlyThisClass onlyThisClass = new ShowOnlyThisClass();
+                        //В зависимости от выбора пользователя, прокидываем в метод
+                        //число, соответствующее определенному классу животного
+                        switch (userChoice2){
+                            case 1:
+                                onlyThisClass.showOnlyThisClass(1);
+                                break;
+                            case 2:
+                                onlyThisClass.showOnlyThisClass(2);
+                                break;
+                            case 3:
+                                onlyThisClass.showOnlyThisClass(3);
+                                break;
+                        }
+                        break;
+                    case 3:
+                        System.out.println("Выбери, по какому параметру ты хочешь отсортировать коллекцию: \n" +
+                                "1. Имя\n" +
+                                "2. Уровень голода\n" +
+                                "3. Вес");
+                        //Считываем пользовательский ввод
+                        int userChoice3 = userChoice.nextInt();
+                        Sorting sorting = new Sorting();
+                        //В зависимости от выбора пользователя, прокидываем в метод
+                        //число, соответствующее определенному классу животного
+                        switch (userChoice3){
+                            case 1:
+                                sorting.sortingAnimals(1);
+                                break;
+                            case 2:
+                                sorting.sortingAnimals(2);
+                                break;
+                            case 3:
+                                sorting.sortingAnimals(3);
+                                break;
+                        }
+                        break;
+                    case 4:
+                        System.out.println("Введи начальную границу диапозона веса животного: ");
+                        //Считываем пользовательский ввод
+                        int choiceWeigthStart = userChoice.nextInt();
+                        System.out.println("Введи конечную границу диапозона веса животного: ");
+                        //Считываем пользовательский ввод
+                        int choiceWeigthEnd = userChoice.nextInt();
+                        ShowInterval showInterval = new ShowInterval();
+                        showInterval.showAnimalsFromInterval(choiceWeigthStart, choiceWeigthEnd);
+                        break;
+                }
             }
+        } catch (Exception e) {
+            System.out.println("Вы ввели что-то не то");
         }
     }
 }
